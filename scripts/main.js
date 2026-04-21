813c800c25f131caee7b9b69768582a2fc316945
Camarederic <camarederic@gmail.com>
2026-04-21 21:29:49 +0300

    Added a JavaScript VideoPlayer module to the video section of the news page JS

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@
 
 import Header from "./Header.js";
 import TabsCollection from "./Tabs.js";
+import VideoPlayerCollection from "./VideoPlayer.js";
 
 new Header();
 new TabsCollection();
+new VideoPlayerCollection();
